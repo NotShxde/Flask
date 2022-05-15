84928e0f3fbeced15797969b654dc898db765ebe
NotShade <38733839+NotShade730@users.noreply.github.com>
2022-05-15 13:22:31 +0530

    add heroku inter
    dd

--- prod/database/index.js
+++ prod/database/index.js
@@ -2,12 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 const pg_1 = require("pg");
+const connectionString = 'postgres://plybsfkzimgkwn:0968ac1d1535315945601db4f90a53d471da7cd40f95c654db0aee2834855069@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d9ggsk373lbj9e'
 const pool = new pg_1.Pool({
-    host: "localhost",
-    port: 5432,
-    user: "notshade",
-    database: 'flask',
-    password: "slq"
+   connectionString,
 });
 pool.connect()
     .then(() => console.log('[INFO]', 'CONNECTION HAS BEEN ESTABLISHED WITH DATABASE'))
