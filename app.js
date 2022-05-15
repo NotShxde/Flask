f3b3595a026f3bac76b9fa06a3471b5c9d08901a
NotShade730 <raiplays10000@gmail.com>
2022-05-15 21:37:21 +0530

    hassa

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-var port = process.env.PORT || 3000;
-app.get("/",(req,res)=>{
-    res.send("Bot Up And Running")
-})
-app.listen(port, function() {
-console.log("Listening on Port 3000");
-});
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,15 @@
 */
 
 import { DiscordRESTError } from "eris";
+const express = require('express')
+const app = express()
+var port = process.env.PORT || 3000;
+app.get("/",(req: any,res:any)=>{
+    res.send("Bot Up And Running")
+})
+app.listen(port, function() {
+console.log("Listening on Port 3000");
+});
 
 const bot = require("./handler/Client.js");
 require('dotenv').config();
