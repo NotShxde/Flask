627e3f9d5641b60d256281153163627a865b4edc
NotShade <38733839+NotShade730@users.noreply.github.com>
2022-05-15 16:55:08 +0530

    Update server.js

--- prod/server.js
+++ prod/server.js
@@ -8,11 +8,6 @@ const express = require('express');
 
 const app = express();
 
-
-app.get('/', (req, res) => {
-  res.send('Hello Express app!')
-});
-
 app.listen(3000, () => {
   console.log('server started');
 });
